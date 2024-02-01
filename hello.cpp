4d18e5473245991d7f3ece5fa3ba7035a8eacb47
jacobrutkowski <rutkow72@msu.edu>
2024-02-01 11:33:00 -0500

    Added tutorial code from class and code for Part 3

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,16 @@
+#include "mpi.h"
 #include <iostream>
 using namespace std;
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-    cout << "Hello, World!" << endl;
+    int numtasks, rank;
+    cout << "Hello, World! (Before Init)" << endl;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    cout << "Hello, World! (After Init, Before Finalize)" << endl;
+    MPI_Finalize();
+    cout << "Hello, World! (After Init)" << endl;
     return 0;
 }
