b75c41dc1828043b9fcb31c17ea2b59a45d21954
jreiling19 <jreiling19@augustana.edu>
2024-02-13 08:11:07 -0500

    Added code for question 2.3

--- hello.cpp
+++ hello.cpp
@@ -6,12 +6,19 @@
 int main(int argc, char *argv[]) 
 {
     int numtasks, rank;
-    printf("Hello, World! (Before Init)\n");
+    //printf("Hello, World! (Before Init)\n");
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    printf("Hello, World! (After Init, Before Finalize)\n");
+    //printf("Hello, World! (After Init, Before Finalize)\n");
+    int name_length = MPI_MAX_PROCESSOR_NAME;
+    char proc_name[name_length];
+    MPI_Get_Processor_name(proc_name, &name_length);
+    //printf("Process %d%d is running on node <<%s>>\n", procid, nprocs, proc_name)
+
+
+
     MPI_Finalize();
-    printf("Hello, World! (After Finalize)\n");
+    //printf("Hello, World! (After Finalize)\n");
     return 0;
 }
