6d78cf24b7ed86aee20be71ec15280b231867174
JReiling1 <reiling1@msu.edu>
2024-02-13 10:31:04 -0500

    adding code for ex. 2.3

--- hello.cpp
+++ hello.cpp
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     //printf("Hello, World! (After Init, Before Finalize)\n");
-    int name_length = MPI_Get_processor_name;
+    int name_length = MPI_MAX_PROCESSOR_NAME;
     char proc_name[name_length];
-    MPI_Get_Processor_name(proc_name, &name_length);
-    //printf("Process %d%d is running on node <<%s>>\n", procid, nprocs, proc_name)
+    MPI_Get_processor_name(proc_name, &name_length);
+    printf("Process %d%d is running on node <<%s>>\n", procid, nprocs, proc_name)
 
 
 
